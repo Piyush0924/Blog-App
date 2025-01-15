866344f28ef34ea643f9b1232b5edf5a1ea0311f
Piyush0924 <147019597+Piyush0924@users.noreply.github.com>
2025-01-15 21:53:54 +0530

    shadow making

--- components/adminComponents/Sidebar.jsx
+++ components/adminComponents/Sidebar.jsx
@@ -14,13 +14,13 @@ const Sidebar = () => {
             {/* navigation buttons*/}
             <div className='w-28 sm:w-80 h-[100vh] relative py-12 border border-black'>
                 <div className='absolute right-0 w-[50%] sm:w-[80%]'>
-                    <Link href='/admin/addProduct' className='flex items-center border border-black gap-3 font-medium py-2 bg-white shadow-[-5px_5px_0px_#000000]'>
+                    <Link href='/admin/addProduct' className='flex items-center border border-black gap-3 font-medium py-2 bg-white shadow-[-7px_0px_0px_#6b7280]'>
                         <Image src={assets.add_icon} width={28} alt='' /><p>Add blogs</p>
                     </Link>
-                    <Link href='/admin/blogList' className='flex items-center border border-black gap-3 mt-5 font-medium py-2 bg-white shadow-[-5px_5px_0px_#000000]'>
+                    <Link href='/admin/blogList' className='flex items-center border border-black gap-3 mt-5 font-medium py-2 bg-white shadow-[-7px_0px_0px_#6b7280]'>
                         <Image src={assets.blog_icon} width={28} alt='' /><p>Blog list</p>
                     </Link>
-                    <Link href='/admin/subscriptions' className='flex items-center border border-black mt-5 gap-3 font-medium py-2 bg-white shadow-[-5px_5px_0px_#000000]'>
+                    <Link href='/admin/subscriptions' className='flex items-center border border-black mt-5 gap-3 font-medium py-2 bg-white shadow-[-7px_0px_0px_#6b7280]'>
                         <Image src={assets.email_icon} width={28} alt='' /><p>Subsriptions</p>
                     </Link>
                 </div>
